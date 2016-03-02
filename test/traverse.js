1a5096a04f1761d7d827a4d94df57abcaf31b1fd
Gvozd <gvozdev.viktor@gmail.com>
2016-03-03 01:46:04 +0300

    return old traverse tests

--- test/traverse.js
+++ test/traverse.js
@@ -3,11 +3,11 @@ import traverse from '../src/traverse';
 import root from '../src/descendant/root';
 import anyLevel from '../src/descendant/any-level';
 import any from '../src/filters/any';
-import {checkerFuncKey} from '../src/const';
 
 describe('traverse', function () {
   'use strict';
-  let object;
+  let anyFilter = anyLevel(any()),
+    object;
   describe('object', function () {
     beforeEach(function () {
       object = {
@@ -25,6 +25,18 @@ describe('traverse', function () {
     it('any element', function () {
       expect(traverse(object, ...anyLevel(any())))
         .deep.equal([object, 'foo', 'bar', object.c, 'quux']);
+    });
+    it('direct order keys', function () {
+      expect(traverse({a: 1, b: 2}, ...anyFilter))
+        .deep.equal([{a: 1, b: 2}, 1, 2]);
+    });
+    it('reverse order keys', function () {
+      expect(traverse({b: 2, a: 1}, ...anyFilter))
+        .deep.equal([{a: 1, b: 2}, 2, 1]);
+    });
+    it('with sub-objects', function () {
+      expect(traverse({a: 1, b: {c: 2}, d: 3}, ...anyFilter))
+        .deep.equal([{a: 1, b: {c: 2}, d: 3}, 1, {c: 2}, 2, 3]);
     });
   });
 
@@ -47,5 +59,14 @@ describe('traverse', function () {
       expect(traverse(object, ...anyLevel(any())))
         .deep.equal([object, 1, 2, [3], 3]);
     });
+    it('plain', function () {
+      expect(traverse([1, 2, 3], ...anyFilter))
+        .deep.equal([[1, 2, 3], 1, 2, 3]);
+    });
+    //it('deeper', function () {
+    //  var notArrayFilter = anyLevel((el)=>({ok: !Array.isArray(el)}));
+    //  expect(traverse([1, 2, [3, [4, 5], 6], 7], notArrayFilter))
+    //    .deep.equal([1, 2, 3, 4, 5, 6, 7]);
+    //});
   });
 });
